68acc2be8d0f70c7e864686ae5963ec36d5a8ac0
k20057 <k20057@yahoo.com.tw>
2022-12-01 15:36:35 +0800

    Initial Commit

---
+++ MovieBooking/MovieBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookingApp.swift
+//  MovieBooking
+//
+//  Created by ming on 2022/12/1.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
