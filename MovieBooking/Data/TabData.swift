5e87aad2e2898cfcf25a1e9960582ffda14dfb93
k20057 <k20057@yahoo.com.tw>
2022-12-10 14:57:49 +0800

    final

---
+++ MovieBooking/Data/TabData.swift
@@ -0,0 +1,16 @@
+//
+//  TabData.swift
+//  MovieBooking
+//
+//  Created by ming on 2022/12/5.
+//
+
+import Foundation
+
+enum Tab: String, CaseIterable {
+    case home = "Home"
+    case location = "Location"
+    case ticket = "Ticket"
+    case category = "Category"
+    case profile = "Profile"
+}
---
+++ MovieBooking/Views/TicketView.swift
@@ -0,0 +1,52 @@
+//
+//  TicketView.swift
+//  MovieBooking
+//
+//  Created by ming on 2022/12/1.
+//
+
+import SwiftUI
+
+struct TicketView: View {
+    @State var animate = false
+    
+    var body: some View {
+        ZStack {
+            CircleBackground(color: Color("greenCircle"))
+                .blur(radius: animate ? 30 : 100)
+                .offset(x: animate ? -50 : -130, y: animate ? -30 : -100)
+                .task {
+                    withAnimation(.easeInOut(duration: 7).repeatForever()) {
+                        animate.toggle()
+                    }
+                }
+            
+            CircleBackground(color: Color("pinkCircle"))
+                .blur(radius: animate ? 30 : 100)
+                .offset(x: animate ? 100 : 130, y: animate ? 150 : 100)
+            
+            VStack(spacing: 30) {
+                Text("Mobile Ticket")
+                    .font(.title3)
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+                
+                Text("Once you buy a movie ticket simply scan the barcode to access to your movie")
+                    .frame(maxWidth: 248)
+                    
+            }
+            .padding(.horizontal, 20)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+            
+            Tickets()
+                .padding(.top, 30)
+        }
+        .background(LinearGradient(gradient: Gradient(colors: [Color("backgroundColor"), Color("backgroundColor2")]), startPoint: .top, endPoint: .bottom))
+    }
+}
+
+struct TicketView_Previews: PreviewProvider {
+    static var previews: some View {
+        TicketView()
+    }
+}
